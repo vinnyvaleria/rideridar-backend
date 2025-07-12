4aeb59a53061fa807c36c0235754f97598aae135
Josephdeepu <josephdeepu1982@gmail.com>
2025-07-12 16:06:31 +0800

    updated the .env & added booking schema

---
+++ daos/bookingDao.js
@@ -0,0 +1,100 @@
+const { number } = require("framer-motion");
+const mongoose = require("mongoose");
+
+const bookingsSchema = new mongoose.Schema(
+  {
+    flights: {
+      number: { type: String, default: "" },
+      terminal: { type: String, default: "" },
+      gate: { type: String, default: "" },
+      luggageNumber: { type: Number, default: 0 },
+      isGatePickupRequested: {
+        type: Boolean,
+        default: false,
+      },
+    },
+    pickupLocation: {
+      type: String,
+      required: true,
+    },
+    dropoffLocation: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    time: {
+      type: String,
+      required: true,
+    },
+    vehicleType: {
+      type: String,
+      enum: ["sedan", "mpv"],
+      required: true,
+    },
+    ridePurpose: {
+      type: String,
+      enum: ["airport", "city", "event", "meeting", "others"],
+      default: "others",
+    },
+    bookingName: {
+      type: String,
+      required: true,
+    },
+    bookingPhone: {
+      type: String,
+      required: true,
+    },
+    bookingEmail: {
+      type: String,
+      required: true,
+    },
+    guestName: {
+      type: String,
+      required: true,
+    },
+    guestPhone: {
+      type: String,
+      required: true,
+    },
+    paxNumber: {
+      type: Number,
+      required: true,
+      default: 1,
+    },
+    specialRequests: {
+      type: String,
+    },
+
+    isDepositTncChecked: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+
+    isDepositPaid: {
+      type: Boolean,
+      default: false,
+    },
+    isAdminTncChecked: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    driverAssigned: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Drivers",
+      default: "unassigned",
+    },
+    status: {
+      type: String,
+      enum: ["pending", "confirmed", "completed", "cancelled"],
+      default: "pending",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Booking", bookingsSchema);
